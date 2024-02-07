627bb8d143051aed7596d3a3b67f49b8d0f932bd
EgorSivenko <egor.siv20@gmail.com>
2024-02-07 22:06:38 +0200

    Cookie logic implemented

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,10 +15,26 @@ import java.time.format.DateTimeFormatter;
 public class TimeServlet extends HttpServlet {
 
     private static final String DEFAULT_TIMEZONE = "UTC";
+    private static final String LAST_TIMEZONE = "lastTimezone";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String timezone = req.getParameter("timezone");
+
+        if (timezone == null) {
+            Cookie[] cookies = req.getCookies();
+            if (cookies != null) {
+                for (var cookie : cookies) {
+                    if (cookie.getName().equals(LAST_TIMEZONE)) {
+                        timezone = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+        } else {
+            Cookie cookie = new Cookie(LAST_TIMEZONE, timezone);
+            resp.addCookie(cookie);
+        }
 
         ZoneId zoneId = ZoneId.of(timezone != null ? timezone : DEFAULT_TIMEZONE);
         ZonedDateTime now = ZonedDateTime.now(zoneId);
